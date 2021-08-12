0c0337c712bd2d91b45e8287ec551d9d8f74d034
Mihail <64327274+Mihahanya@users.noreply.github.com>
2021-08-12 21:46:58 +0300

    Add files via upload

---
+++ rus.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#define цлчис int
+#define дробн double
+#define логич bool
+#define конст const
+#define огромный unsigned
+#define строка std::string
+
+#define Правда true
+#define Ложь false
+
+#define доб <<
+#define пер >>
+
+#define нч {
+#define кц }
+
+#define нчГлавный int main() { SetConsoleCP(1251); SetConsoleOutputCP(1251);
+#define кцГлавный }
+
+#define если if
+#define иначе else
+
+#define консоль std::cout
+#define ввод std::cin
+#define кцстр "\n"
+
+#define для for
+#define пока while
+
+#define длина length
+#define мин min
+#define макс max
+#define син sin
+#define кос cos
+#define степ pow
+#define кор sqrt
+#define мод abs
+#define окрвн floor
+
+#define вернуть return
+#define прервать break
+
+#define Система system
+#define Задержка Sleep
+#define таймер clock
